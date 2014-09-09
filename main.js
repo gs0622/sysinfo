cd97567a8b23441f532ddc06061ba44c21af05f7
Harry Pan <gs0622@gmail.com>
2014-09-09 17:42:53 +0800

    add cpu modelName/archName/features, and user time usage in %

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 
 window.onload = function() {
-  var greeting = document.querySelector('#greeting');
-  greeting.innerText = 'Hello, World! It is ' + new Date() + '\n';
+  //var greeting = document.querySelector('#greeting');
+  //greeting.innerText = 'Hello, World! It is ' + new Date() + '\n';
+  var cpu = document.querySelector('#info');
   chrome.system.cpu.getInfo(function(c) {
-    for (var i = 0; i < c.numOfProcessors; i++) {
-      var user = c.processors[i].usage.user/c.processors[i].usage.total;
-      greeting.innerText += 'cpu' + i + ':' + Math.floor(user*100) + '\n';
-    }
+    cpu.innerText += c.modelName + '\n';
+    cpu.innerText += c.archName + '\n';
+    cpu.innerText += c.features + '\n';
   });
-  //window.setInterval(UpdateCpu, 3000);
+
+  chrome.system.cpu.getInfo(function(c) {
+    OldInfo = c;
+  });
+
+  window.setInterval(UpdateCpu, 2000);
 };
 
+var OldInfo;
+
+function UpdateCpu() {
+  var cpu = document.querySelector('#usage');
+  cpu.innerText = '';
+  chrome.system.cpu.getInfo(function(c) {
+    for (var i = 0; i < c.numOfProcessors; i++) {
+      var total = c.processors[i].usage.total - OldInfo.processors[i].usage.total;
+      var user = c.processors[i].usage.user - OldInfo.processors[i].usage.user;
+      cpu.innerText += 'cpu' + i + ': ' + Math.floor(user/total*100) + '\n';
+    }
+    OldInfo = c;
+  });
+}
