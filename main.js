87fb9dd1d8193e65af956d62d73a43ce87cb643b
Harry Pan <gs0622@gmail.com>
2014-09-11 00:09:45 +0800

    add kernel/idle cputime, plus chrome.system.memory api example

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
+var old; // Old CpuInfo
 
 window.onload = function() {
-  //var greeting = document.querySelector('#greeting');
-  //greeting.innerText = 'Hello, World! It is ' + new Date() + '\n';
   var cpu = document.querySelector('#info');
   chrome.system.cpu.getInfo(function(c) {
-    cpu.innerText += c.modelName + '\n';
-    cpu.innerText += c.archName + '\n';
-    cpu.innerText += c.features + '\n';
+    cpu.innerHTML += '<tr><th width="25%">model name</th><td>' + c.modelName + '</td></tr>';
+    cpu.innerHTML += '<tr><th>arch name</th><td>' + c.archName + '</td></tr>';
+    cpu.innerHTML += '<tr><th>feature</th><td>' + c.features + '</td></tr>';
   });
 
   chrome.system.cpu.getInfo(function(c) {
-    OldInfo = c;
+    old = c;
   });
 
-  window.setInterval(UpdateCpu, 2000);
+  window.setInterval(UpdateAll, 2000);
 };
-
-var OldInfo;
 
 function UpdateCpu() {
   var cpu = document.querySelector('#usage');
-  cpu.innerText = '';
+  cpu.innerHTML = '<tr><th width="25%">cpu</th>' +
+                  '<th width="25%">user</th>' +
+                  '<th width="25%">kernel</th>' +
+                  '<th width="25%">idle</th></tr>';
   chrome.system.cpu.getInfo(function(c) {
     for (var i = 0; i < c.numOfProcessors; i++) {
-      var total = c.processors[i].usage.total - OldInfo.processors[i].usage.total;
-      var user = c.processors[i].usage.user - OldInfo.processors[i].usage.user;
-      cpu.innerText += 'cpu' + i + ': ' + Math.floor(user/total*100) + '\n';
+      var total = c.processors[i].usage.total - old.processors[i].usage.total;
+      var user = c.processors[i].usage.user - old.processors[i].usage.user;
+      var kernel = c.processors[i].usage.kernel - old.processors[i].usage.kernel;
+      var idle = c.processors[i].usage.idle - old.processors[i].usage.idle;
+      cpu.innerHTML += '<tr><th>' + i + '</th>' +
+                       '<td>' + Math.round(user/total*100) + '</td>' +
+                       '<td>' + Math.round(kernel/total*100) + '</td>' +
+                       '<td>' + Math.round(idle/total*100) + '</td></tr>';
     }
-    OldInfo = c;
+    old = c;
   });
+}
+
+function UpdateMem() {
+  var mem = document.querySelector('#memory');
+  mem.innerHTML = '<tr><th width="50%">total (MiB) </th><th>available (MiB)</th></tr>';
+  chrome.system.memory.getInfo(function(m) {
+    mem.innerHTML += '<tr><td>' + Math.round(m.capacity/1048576) +
+                     '</td><td>' + Math.round(m.availableCapacity/1048576) +
+                     '</td></tr>';
+  });
+}
+
+function UpdateAll() {
+  UpdateCpu();
+  UpdateMem();
 }
