f98613cea9893ba5833d90e8f292c5610f35468d
Harry Pan <gs0622@gmail.com>
2014-09-11 19:41:10 +0800

    add UpdateAll call in onload to plot table first

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var old; // Old CpuInfo
 window.onload = function() {
   var cpu = document.querySelector('#info');
   chrome.system.cpu.getInfo(function(c) {
-    cpu.innerHTML += '<tr><th width="25%">model name</th><td>' + c.modelName + '</td></tr>';
+    cpu.innerHTML += '<tr><th width="25%">model name</th><td>' + c.modelName +
+                     '</td></tr>';
     cpu.innerHTML += '<tr><th>arch name</th><td>' + c.archName + '</td></tr>';
     cpu.innerHTML += '<tr><th>feature</th><td>' + c.features + '</td></tr>';
   });
@@ -11,7 +12,7 @@ window.onload = function() {
   chrome.system.cpu.getInfo(function(c) {
     old = c;
   });
-
+  UpdateAll();
   window.setInterval(UpdateAll, 2000);
 };
 
@@ -25,7 +26,8 @@ function UpdateCpu() {
     for (var i = 0; i < c.numOfProcessors; i++) {
       var total = c.processors[i].usage.total - old.processors[i].usage.total;
       var user = c.processors[i].usage.user - old.processors[i].usage.user;
-      var kernel = c.processors[i].usage.kernel - old.processors[i].usage.kernel;
+      var kernel = c.processors[i].usage.kernel -
+                   old.processors[i].usage.kernel;
       var idle = c.processors[i].usage.idle - old.processors[i].usage.idle;
       cpu.innerHTML += '<tr><th>' + i + '</th>' +
                        '<td>' + Math.round(user/total*100) + '</td>' +
@@ -38,7 +40,8 @@ function UpdateCpu() {
 
 function UpdateMem() {
   var mem = document.querySelector('#memory');
-  mem.innerHTML = '<tr><th width="50%">total (MiB) </th><th>available (MiB)</th></tr>';
+  mem.innerHTML = '<tr><th width="50%">total (MiB)'+
+                  '</th><th>available (MiB)</th></tr>';
   chrome.system.memory.getInfo(function(m) {
     mem.innerHTML += '<tr><td>' + Math.round(m.capacity/1048576) +
                      '</td><td>' + Math.round(m.availableCapacity/1048576) +
